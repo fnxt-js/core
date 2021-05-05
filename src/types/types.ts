7b4c6409264d9e2e50c8c56ded01048403c9c8ae
mb <mb@base10.at>
2021-05-05 23:36:57 +0200

    improve types

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,10 +2,11 @@ export type Thunk<R> = () => R;
 export type UnaryFunction<E, R> = (e: E) => R;
 export type BinaryFunction<E, F, R> = (e: E, f: F) => R;
 
-export type KeyProjection<E> = UnaryFunction<E, string | number>;
+export type NumberProjection<E> = UnaryFunction<E, number>;
+export type KeyProjection<E> = UnaryFunction<E, string> | NumberProjection<E>;
 export type Predicate<E> = (e: E) => boolean;
 export type Reduction<E> = (e: E, f: E) => E;
-export type Folder<E, F> = (e: F, f: E) => F;
+export type Folder<E, F> = (f: F, e: E) => F;
 
 
 export type Tuple<E, F> = [e: E, f: F];
