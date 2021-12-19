d57164651dae510818ea8fa93dc60018bfb0e51c
mb <mb@base10.at>
2021-12-19 17:04:32 +0100

    add mutation type

--- src/fnxt-types/types.ts
+++ src/fnxt-types/types.ts
@@ -2,14 +2,19 @@ export type Thunk<R> = () => R;
 export type UnaryFunction<E, R> = (e: E) => R;
 export type BinaryFunction<E, F, R> = (e: E, f: F) => R;
 
+
+
 export type NumberProjection<E> = UnaryFunction<E, number>;
 export type KeyProjection<E> = UnaryFunction<E, string> | NumberProjection<E>;
 export type Predicate<E> = (e: E) => boolean;
-export type Comparer<E> = (e: E, f: E) => number;
-export type Reduction<E> = (e: E, f: E) => E;
-export type Folder<E, F> = (f: F, e: E) => F;
+export type Comparer<E> = BinaryFunction<E, E, number>;
+export type Reduction<E> = BinaryFunction<E, E, E>;
+export type Folder<E, F> = BinaryFunction<F, E, F>;
+export type Mutation<E> = UnaryFunction<E, E>;
 
-export interface Dictionary<E> { [name: string]: E }
+export interface Dictionary<E> {
+    [name: string]: E;
+}
 
 
 export type Tuple<E, F> = [E, F];
