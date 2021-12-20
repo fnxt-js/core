facf2a54809b31fd2daf1d7d9c241569925503dc
mb <mb@base10.at>
2021-12-20 19:59:06 +0100

    fix allCombinations type

--- src/array/operator/allCombinations.ts
+++ src/array/operator/allCombinations.ts
@@ -2,7 +2,16 @@ import {allPairs} from './allPairs';
 import {map} from './map';
 import {pipe} from 'fnxt/pipe';
 
-export const allCombinations = (...arrays: unknown[][]): unknown[][] => {
+export function allCombinations<A>(a: A[]): [A][];
+export function allCombinations<A, B>(a: A[], b: B[]): [A, B][];
+export function allCombinations<A, B, C>(a: A[], b: B[], c: C[]): [A, B, C][];
+export function allCombinations<A, B, C, D>(a: A[], b: B[], c: C[], d: D[]): [A, B, C, D][];
+export function allCombinations<A, B, C, D, E>(a: A[], b: B[], c: C[], d: D[], e: E[]): [A, B, C, D, E][];
+export function allCombinations<A, B, C, D, E, F>(a: A[], b: B[], c: C[], d: D[], e: E[], f: F[]): [A, B, C, D, E, F][];
+export function allCombinations<A, B, C, D, E, F, G>(a: A[], b: B[], c: C[], d: D[], e: E[], f: F[], g: G[]): [A, B, C, D, E, F, G][];
+export function allCombinations<A, B, C, D, E, F, G, H>(a: A[], b: B[], c: C[], d: D[], e: E[], f: F[], g: G[], h: H[]): [A, B, C, D, E, F, G, H][];
+
+export function allCombinations(...arrays: unknown[][]): unknown[][] {
 
     const fn = ([head, ...tail]: unknown[][]): unknown[][] =>
         tail.length
@@ -10,5 +19,5 @@ export const allCombinations = (...arrays: unknown[][]): unknown[][] => {
             : head.map(e => [e]);
     return fn(arrays);
 
-};
+}
 
