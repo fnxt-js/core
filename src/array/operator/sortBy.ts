aba35a9f1d24f26761c853c42eeedff266588c66
mb <mb@base10.at>
2022-09-18 22:59:37 +0200

    1.5.0

--- src/array/operator/sortBy.ts
+++ src/array/operator/sortBy.ts
@@ -4,7 +4,7 @@ export const sortBy = <T>(fn: KeyProjection<T>) => (array: T[]): T[] => {
   const tuples = array.map(e => [fn(e), e]);
   return (tuples
     // @ts-ignore
-    .sort((tuples[0] && typeof tuples[0][0] == 'number')
+    .sort((tuples[0] && typeof tuples[0][0] === 'number')
       ? ((a: [number, T], b: [number, T]) => a[0] - b[0])
       : ((a: [string, T], b: [string, T]) => a[0].localeCompare(b[0]))
     ) as [number, T][] | [string, T][])
--- src/array/operator/sortByDescending.ts
+++ src/array/operator/sortByDescending.ts
@@ -4,7 +4,7 @@ export const sortByDescending = <T>(fn: KeyProjection<T>) => (array: T[]): T[] =
   const tuples = array.map(e => [fn(e), e]);
   return (tuples
     // @ts-ignore
-    .sort((tuples[0] && typeof tuples[0][0] == 'number')
+    .sort((tuples[0] && typeof tuples[0][0] === 'number')
       ? ((a: [number, T], b: [number, T]) => b[0] - a[0])
       : ((a: [string, T], b: [string, T]) => b[0].localeCompare(a[0]))
     ) as [number, T][] | [string, T][])
