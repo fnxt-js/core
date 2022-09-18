2fe11b27231f88c6c8b150687df700ee09e05bb5
mb <mb@base10.at>
2022-09-18 23:08:26 +0200

    1.5.1 fix skip generic

--- src/array/operator/skip.ts
+++ src/array/operator/skip.ts
@@ -1,4 +1,4 @@
-export const skip = <T>(count: number) => (array: T[]): T[] => {
+export const skip = (count: number) => <T>(array: T[]): T[] => {
   const result = [];
   for (let i = count; i < array.length; i++) {
     result.push(array[i]);
