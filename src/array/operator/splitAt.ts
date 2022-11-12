f9c09e384470f8b017fa479d819698e1b8e05f4e
mb <mb@base10.at>
2022-11-12 19:33:10 +0100

    use slice for splitAt

--- src/array/operator/splitAt.ts
+++ src/array/operator/splitAt.ts
@@ -1,17 +1,5 @@
 import {Tuple} from 'fnxt/fnxt-types';
 
-export const splitAt = <T>(index: number) => (array: T[]): Tuple<T[], T[]> => {
-  if (array.length <= index) {
-    return [array, []];
-  }
-  const a = [];
-  const b = [];
-  for (let i = 0; i < index; i++) {
-    a.push(array[i]);
-  }
-  for (let i = index; i < array.length; i++) {
-    b.push(array[i]);
-  }
-  return [a, b];
-
-};
+export const splitAt = <T>(index: number) =>
+  (array: T[]): Tuple<T[], T[]> =>
+    [array.slice(0, index), array.slice(index)];
