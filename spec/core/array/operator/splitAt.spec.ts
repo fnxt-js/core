545641ba5bde5d3bb52929f8eb8a890851bd1607
mb <mb@base10.at>
2023-01-07 23:48:01 +0100

    fix type of splitAt

--- spec/core/array/operator/splitAt.spec.ts
+++ spec/core/array/operator/splitAt.spec.ts
@@ -5,33 +5,33 @@ import {splitAt} from '../../../../src/array';
 describe('splitAt', () => {
   it('should splitAt', () => {
     const array = [1, 2, 3, 4];
-    const fn = splitAt<number>(2);
+    const fn = splitAt(2);
     expect(fn(array)).to.eql([[1, 2], [3, 4]]);
 
   });
   it('should splitAt empty', () => {
-    const fn = splitAt<number>(2);
+    const fn = splitAt(2);
     expect(fn([])).to.eql([[], []]);
   });
 
   it('should splitAt early', () => {
-    const fn = splitAt<number>(2);
+    const fn = splitAt(2);
     expect(fn([1])).to.eql([[1], []]);
   });
   it('should splitAt last', () => {
-    const fn = splitAt<number>(2);
+    const fn = splitAt(2);
     expect(fn([1, 2])).to.eql([[1, 2], []]);
   });
 
   it('should splitAt 0', () => {
     const array = [1, 2, 3, 4];
-    const fn = splitAt<number>(0);
+    const fn = splitAt(0);
     expect(fn(array)).to.eql([[], [1, 2, 3, 4]]);
   });
 
 
   it('should throw if null or undefined', () => {
-    const fn = splitAt<number>(2);
+    const fn = splitAt(2);
     checkThrow(fn);
   });
 });
--- src/array/operator/splitAt.ts
+++ src/array/operator/splitAt.ts
@@ -1,5 +1,5 @@
 import {Tuple} from 'fnxt/fnxt-types';
 
-export const splitAt = <T>(index: number) =>
-  (array: T[]): Tuple<T[], T[]> =>
+export const splitAt = (index: number) =>
+  <T>(array: T[]): Tuple<T[], T[]> =>
     [array.slice(0, index), array.slice(index)];
