001ace28068ec802e8e2eefa57a3ce056842653b
mb <mb@base10.at>
2023-09-22 16:21:55 +0200

    fix tests

--- spec/core/array/operator/flatten.spec.ts
+++ spec/core/array/operator/flatten.spec.ts
@@ -2,7 +2,7 @@ import {expect} from 'chai';
 import {flatten} from '../../../../src/array';
 import {checkThrow} from '../../../support/checkThrow';
 
-describe.only('flatten', () => {
+describe('flatten', () => {
     it('should flatten', () => {
         expect(flatten([[1], [2, 3], [4]])).to.eql([1, 2, 3, 4]);
         expect(flatten([])).to.eql([]);
